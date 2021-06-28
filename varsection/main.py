7c6179157cb62c67c953604dbcd7c355d03fb9c4
Beimnet B Degefu <beimdegefu@gmail.com>
2021-06-28 12:05:55 +0300

    variables commit

---
+++ varsection/main.py
@@ -0,0 +1,34 @@
+# this is list of audio file attributes for pirple variables test one
+"""
+	This is repeating what is written in above to show that it can be written
+	using multiline comment, and the comment is:-
+
+		\"this is list of audio file attributes for pirple variables test one\"
+"""
+
+#declaring variables with values
+
+title="Egmont Overture"
+Artist="Beethoven"
+Album="The Very Best Of Beethoven"
+Genre="Classical"
+Year=2005
+Track_Number=9
+Duration=0.14 # in hours or it is about 504 seconds
+Size="16MB"
+Type="MP3 Audio"
+SAMPLE_RATE= 44100 # this is in HZ(Hertz)
+ 
+
+
+#now printing the variables
+
+print(title)
+print(Artist) 
+print(Album)
+print(Genre)
+print(Year)
+print(Track_Number)
+print(Duration)
+print(Size)
+print(SAMPLE_RATE)
